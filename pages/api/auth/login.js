b731bf323628e323703e525d95791f54a162e74a
zafar26 <qutubuddinzafar2616118@gmail.com>
2024-01-08 19:44:41 +0530

    Changed MAthod & its Condition

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -3,21 +3,24 @@ import connect from "@/db"
 import { createToken } from "@/utils/helper";
 
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        let info = req.body;
-        if(info.username && info.password){
-            try{
-                await connect()
-                let user = await User.findOne({username: info.username })
-                if(user.verifyPasswordSync(info.password)){
-                    let token = createToken(user._id)
-                    res.status(200).json({token, message:"Loged In Succes"})
-                }
-                res.status(404).json({ error: 'Wrong Password ' })
-            } catch(err){
-                res.status(500).json({ error:err, message: 'failed to Login' })
-            }
-        }
-        res.status(404).json({ error: 'User Name | Password Missing' })
+    if (req.method !== 'POST') {
+        res.status(405).send({ message: 'Only POST requests allowed' })
+        return
     }
+    let info = req.body;
+    if(info.username && info.password){
+        try{
+            await connect()
+            let user = await User.findOne({username: info.username })
+            if(user.verifyPasswordSync(info.password)){
+                let token = createToken(user._id)
+                res.status(200).json({token, message:"Loged In Succes"})
+            }
+            res.status(404).json({ error: 'Wrong Password ' })
+        } catch(err){
+            res.status(500).json({ error:err, message: 'failed to Login' })
+        }
+    }
+    res.status(404).json({ error: 'User Name | Password Missing' })
+
 }
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -3,23 +3,25 @@ import connect from "@/db"
 import { createToken } from "@/utils/helper";
 
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        let info = req.body;
-        if(info.username && info.password){
-            try{
-                await connect()
-
-                let new_user = new User(info)
-                await new_user.save();
-                
-                let token = createToken(new_user._id)
-                
-                res.status(200).json({token, message:"Signedup Succesfull"})
-            } catch(err){
-                res.status(500).json({ error: err,
-                    message:'failed to Signup' })
-            }
-        }
-        res.status(404).json({ error: 'User Name | Password Missing' })
+    if (req.method !== 'POST') {
+        res.status(405).send({ message: 'Only POST requests allowed' })
+        return
     }
+    let info = req.body;
+    if(info.username && info.password){
+        try{
+            await connect()
+
+            let new_user = new User(info)
+            await new_user.save();
+            
+            let token = createToken(new_user._id)
+            
+            res.status(200).json({token, message:"Signedup Succesfull"})
+        } catch(err){
+            res.status(500).json({ error: err,
+                message:'failed to Signup' })
+        }
+    }
+    res.status(404).json({ error: 'User Name | Password Missing' })
 }
