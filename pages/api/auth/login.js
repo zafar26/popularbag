17cb7e96f7be69791b22d7a1e3597d8d5c266417
zafar26 <qutubuddinzafar2616118@gmail.com>
2024-01-09 13:01:05 +0530

    Added Some CORS

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,8 +1,14 @@
 import User from "@/utils/models/User";
 import connect from "@/db"
 import { createToken } from "@/utils/helper";
-import NextCors from 'nextjs-cors';
+// import NextCors from 'nextjs-cors';
+import Cors from 'cors'
 
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+})
 export const config = {
     api: {
       externalResolver: true,
@@ -38,27 +44,39 @@ export const config = {
     fn
   ) {
     return new Promise((resolve, reject) => {
+        console.log("Before Promise")
       fn(req, res, (result) => {
+        console.log("Before Cond")
+
         if (result instanceof Error) {
+        console.log("Cond")
+
           return reject(result)
         }
-  
+        console.log("Before Return")
+        
         return resolve(result)
       })
+      console.log("After Promise")
+
     })
   }
 
 export default async function login(req, res) {
     try{
-        
-        let cors = await NextCors(req, res, {
-            // Options
-            methods: [ 'POST' ],
-            origin: '*',
-            optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-        });
+        console.log("Before CORS")
+        // let cors = await NextCors(req, res, {
+        //     // Options
+        //     methods: [ 'POST' ],
+        //     origin: '*',
+        //     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+        // });
+        console.log("Before Middleware")
+
         // Run the middleware
         await runMiddleware(req, res, cors)
+        console.log("After Middleware")
+
     } catch(er){
            res.status(400).json({message: "FAILED BEFORE"}) 
     }
