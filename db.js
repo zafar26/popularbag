325f00b5feffb6f7776336c7c4567bfd36a042fd
zafar26 <qutubuddinzafar2616118@gmail.com>
2024-01-09 19:34:45 +0530

    Added Cached Mongoose Conect func

--- db.js
+++ db.js
@@ -1,14 +1,38 @@
 import mongoose from "mongoose";
 
-const connect = async()=>{
-    try {
-        if(mongoose.connection.readyState != 1){
-            await mongoose.connect (process.env.MONGO_URL);
-        }
-    } catch (errer) {
-        console.log(error, "ERROR Conecting Mongo")
-        throw new Error("Error in connecting to mongodb.");
-    }
+const MONGODB_URI = process.env.MONGO_URL ;
+
+if (!MONGODB_URI) throw new Error("MONGODB_URI not defined");
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    cached.promise = mongoose
+      .connect(MONGODB_URI, opts)
+      .then((mongoose) => {
+        return mongoose;
+      });
+  }
+  try {
+    cached.conn = await cached.promise;
+    
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
 }
 
 export default connect;
